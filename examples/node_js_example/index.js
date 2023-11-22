6ed42bb5eee9665ad0a680261cb55c1d7a0adec8
lesleysin <nikandsin@gmail.com>
2023-11-22 22:48:15 +0200

    Examples updated

--- examples/node_js_example/index.js
+++ examples/node_js_example/index.js
@@ -1,6 +1,13 @@
 const http = require("http");
-const express = require( "express");
-const WebSocket = require( "ws");
+const express = require("express");
+const WebSocket = require("ws");
+const { DuitView, DuitElementType, Widgets, ColoredBoxUiElement } = require("duit_js");
+const { WebSocketAction } = require("duit_js");
+const { SizedBoxUiElement } = require("duit_js");
+const { TextUiElement } = require("duit_js");
+const { CenterUiElement } = require("duit_js");
+const { RowUiElement } = require("duit_js");
+const { ElevatedButtonUiElement } = require("duit_js");
 
 const app = express();
 
@@ -8,31 +15,32 @@ const server = http.createServer(app);
 
 const webSocketServer = new WebSocket.Server({ server });
 
-webSocketServer.on('connection', ws => {
+function createDynamicDuitView() {
+   //create UIBuilder instance
+   const builder = DuitView.builder();
 
+   //create child elements tree
+   const sizedBoxWithCentredText = new RowUiElement({ mainAxisAlignment: "spaceEvenly" }).addChild(new SizedBoxUiElement({ width: 100, height: 400 }).addChild(new ColoredBoxUiElement({ color: "#DCDCDC" }).addChild(new CenterUiElement({}).addChild(new TextUiElement({ data: "1123" }))))).addChild(new SizedBoxUiElement({ width: 120, height: 300 }).addChild(new ColoredBoxUiElement({ color: "#9e2f2f" }).addChild(new CenterUiElement({}).addChild(new ElevatedButtonUiElement({}, "button1", new WebSocketAction("event1", [])).addChild(new TextUiElement({ data: "1" }))))))
+
+   //create view root and assing child/children to him
+   builder.createRootOfExactType(DuitElementType.column, {}).addChild(sizedBoxWithCentredText);
+
+   //return json string
+   return builder.build();
+}
+
+
+webSocketServer.on('connection', ws => {
    ws.on('message', m => {
-webSocketServer.clients.forEach(client => client.send(m));
+      webSocketServer.clients.forEach(client => client.send(m));
    });
 
    ws.on("error", e => ws.send(e));
 
    setTimeout(() => {
-    ws.send(JSON.stringify({
-      "type": "Text",
-      "uncontrolled": false,
-      "id": "ddf",
-      "attributes": {"data": "TEST TEXT"},
-      "action": {},
-    }));
-   }, 3000);
+      ws.send(createDynamicDuitView());
 
-   setTimeout(() => {
-    ws.send(JSON.stringify({
-      "updates": {
-        "ddf": {"data": "received!"}
-      }
-    }));
-   }, 5000);
+   }, 3000);
 });
 
 server.listen(8999, () => console.log("Server started"))
