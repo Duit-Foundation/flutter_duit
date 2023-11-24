4071041f8c76faa2a05b1ed0e59717c4b20a10f3
lesleysin <nikandsin@gmail.com>
2023-11-24 18:46:21 +0200

    [js] events export

--- packages/duit_js/src/index.ts
+++ packages/duit_js/src/index.ts
@@ -2,4 +2,5 @@ export { HttpAction, WebSocketAction } from "./lib/action";
 export { default as DuitView } from "./lib/duit_view";
 export { DuitElement, DuitLayoutElement } from "./lib/element";
 export { default as DuitElementType } from "./lib/element_type";
-export * from "./widget_models/index"
+export * from "./lib/event";
+export * from "./widget_models/index";
