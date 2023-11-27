208811e2b95edeb50a93f529969262a87d9d4810
lesleysin <nikandsin@gmail.com>
2023-11-27 16:54:48 +0200

    completely setup go package

--- examples/go_example/go.mod
+++ examples/go_example/go.mod
@@ -1,3 +1,5 @@
 module goexample
 
 go 1.20
+
+require github.com/lesleysin/duit v0.0.0-20231127142042-7390ed3de03c
--- examples/go_example/main.go
+++ examples/go_example/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/lesleysin/duit/packages/duit_go"
 
+func main() {
+	duit_go.Test()
 }
