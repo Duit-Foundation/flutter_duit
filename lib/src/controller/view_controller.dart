d58896e25ecdf7a1533c10a576f7ea19c9c24ea5
lesleysin <nikandsin@gmail.com>
2023-12-08 13:37:20 +0200

    Added documentation for controller package

--- lib/src/controller/view_controller.dart
+++ lib/src/controller/view_controller.dart
@@ -5,28 +5,57 @@ import 'package:flutter_duit/src/ui/models/el_type.dart';
 
 import 'ui_controller.dart';
 
-final class ViewController<T>
-    with ChangeNotifier
-    implements UIElementController<T> {
-  //<editor-fold desc="Properties and ctor">
+/// The controller for a UI element.
+///
+/// This class is responsible for managing the state and behavior of a UI element.
+/// It implements the [UIElementController] interface and uses the [ChangeNotifier]
+/// mixin to provide change notification to listeners.
+final class ViewController<T> with ChangeNotifier implements UIElementController<T> {
+  /// The attributes associated with the UI element.
+  ///
+  /// This property holds the attributes of the UI element that the `ViewController` controls.
+  /// It can be used to access and modify the attributes of the UI element.
   @override
   ViewAttributeWrapper<T>? attributes;
 
+  /// The server action associated with the UI element.
+  ///
+  /// This property holds the server action that is triggered when the UI element is interacted with.
   @override
   ServerAction? action;
 
+  /// The driver that controls the UI element.
+  ///
+  /// This property holds the driver object that is responsible for interacting with the UI element.
   @override
   UIDriver driver;
 
+  /// The unique identifier of the UI element.
+  ///
+  /// This property holds the unique identifier that is assigned to the UI element.
   @override
   String id;
 
+  /// The type of the UI element.
+  ///
+  /// This property holds the type of the UI element that the `ViewController` controls.
   @override
   DUITElementType type;
 
+  /// The tag associated with the UI element.
+  ///
+  /// This property holds an optional tag that can be used to further categorize the UI element.
   @override
   String? tag;
 
+  /// Creates a new instance of the `ViewController` class.
+  ///
+  /// The [id] parameter specifies the unique identifier of the UI element.
+  /// The [driver] parameter specifies the driver that controls the UI element.
+  /// The [type] parameter specifies the type of the UI element.
+  /// The [action] parameter specifies the server action associated with the UI element.
+  /// The [attributes] parameter specifies the initial attributes of the UI element.
+  /// The [tag] parameter specifies the tag associated with the UI element.
   ViewController({
     required this.id,
     required this.driver,
@@ -36,20 +65,23 @@ final class ViewController<T>
     this.tag,
   });
 
-  //</editor-fold>
-
-  //<editor-fold desc="Methods">
+  /// Updates the state of the UI element with new attributes.
+  ///
+  /// The [newAttrs] parameter specifies the new attributes to be applied to the UI element.
   @override
   void updateState(ViewAttributeWrapper<T> newAttrs) {
     attributes = newAttrs;
     notifyListeners();
   }
 
+  /// Performs the related action of the UI element.
+  ///
+  /// This method is called when the UI element is interacted with and the associated
+  /// server action is not null. It executes the server action using the driver.
   @override
   void performRelatedAction() {
     if (action != null) {
       driver.execute(action!);
     }
   }
-//</editor-fold>
 }
