ff70a0cabd82c613c2e6b3b1e17e9cc3ca7218c1
lesleysin <nikandsin@gmail.com>
2024-10-03 12:44:05 +0200

    Fix test one more time

--- test/d_component_test.dart
+++ test/d_component_test.dart
@@ -96,32 +96,83 @@ void main() {
     builder: () => GoldenTestGroup(children: [
       GoldenTestScenario(
         name: "Component base scenario",
-        child: DuitViewHost(
-          driver: DuitDriver.static(
-            {
-              "type": "Component",
-              "id": "comp1",
-              "tag": "x",
-              "data": componentTemplateData,
-            },
-            transportOptions: HttpTransportOptions(),
-            enableDevMetrics: false,
-          ),
+        child: Column(
+          children: [
+            DuitViewHost(
+              driver: DuitDriver.static(
+                {
+                  "type": "Component",
+                  "id": "comp1",
+                  "tag": "x",
+                  "data": componentTemplateData,
+                },
+                transportOptions: HttpTransportOptions(),
+                enableDevMetrics: false,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+            ),
+          ],
         ),
       ),
       GoldenTestScenario(
         name: "Component with default value",
-        child: DuitViewHost(
-          driver: DuitDriver.static(
-            {
-              "type": "Component",
-              "id": "comp1",
-              "tag": "x",
-              "data": componentTemplateData2,
-            },
-            transportOptions: HttpTransportOptions(),
-            enableDevMetrics: false,
-          ),
+        child: Column(
+          children: [
+            DuitViewHost(
+              driver: DuitDriver.static(
+                {
+                  "type": "Component",
+                  "id": "comp1",
+                  "tag": "x",
+                  "data": componentTemplateData2,
+                },
+                transportOptions: HttpTransportOptions(),
+                enableDevMetrics: false,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+      GoldenTestScenario(
+        name: "Component with non existent tag",
+        child: Column(
+          children: [
+            DuitViewHost(
+              driver: DuitDriver.static(
+                {
+                  "type": "Component",
+                  "id": "comp1",
+                  "tag": "invalid_tag",
+                  "data": componentTemplateData2,
+                },
+                transportOptions: HttpTransportOptions(),
+                enableDevMetrics: false,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+            ),
+          ],
         ),
       ),
     ]),
