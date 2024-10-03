921a8c7ee123dedbdfd4e4dbd5cd7cbbfe79e150
lesleysin <nikandsin@gmail.com>
2024-10-03 10:05:47 +0200

    Fix test config

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -6,9 +6,13 @@ import "package:alchemist/alchemist.dart";
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   HttpOverrides.global = null;
 
+  const isRunningInCi = bool.fromEnvironment('CI', defaultValue: false);
+
   return AlchemistConfig.runWithConfig(
     config: const AlchemistConfig(
-      ciGoldensConfig: CiGoldensConfig(),
+      ciGoldensConfig: CiGoldensConfig(
+        enabled: !isRunningInCi
+      ),
     ),
     run: testMain,
   );
