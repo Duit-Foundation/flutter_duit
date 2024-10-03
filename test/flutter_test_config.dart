a12613f0aa0ec4c4424c60cbf67c1012b66edb97
lesleysin <nikandsin@gmail.com>
2024-10-03 09:37:23 +0200

    Migrate to alchemist package

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,9 +1,7 @@
 import "dart:async";
 import "dart:io";
-import "package:golden_toolkit/golden_toolkit.dart";
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   HttpOverrides.global = null;
-  await loadAppFonts();
   return testMain();
 }
