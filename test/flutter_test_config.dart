316b96c0cd6c8a23c8ce339f34075bf358ff860a
lesleysin <nikandsin@gmail.com>
2024-10-03 09:48:41 +0200

    Upd test config

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,7 +1,15 @@
 import "dart:async";
 import "dart:io";
 
+import "package:alchemist/alchemist.dart";
+
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   HttpOverrides.global = null;
-  return testMain();
+
+  return AlchemistConfig.runWithConfig(
+    config: const AlchemistConfig(
+      ciGoldensConfig: CiGoldensConfig(),
+    ),
+    run: testMain,
+  );
 }
