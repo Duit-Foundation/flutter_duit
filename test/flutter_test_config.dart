b63beb58fdbb204e43fd16a1e86f1ea093cd7f2e
lesleysin <nikandsin@gmail.com>
2024-10-03 10:22:41 +0200

    Fix config!!!!

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -6,16 +6,13 @@ import "package:alchemist/alchemist.dart";
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   HttpOverrides.global = null;
 
-  const isRunningInCi = bool.fromEnvironment('CI');
+  final isGithubActionsEnv = Platform.isLinux;
 
   return AlchemistConfig.runWithConfig(
-    config: const AlchemistConfig(
-      platformGoldensConfig: PlatformGoldensConfig(
-        enabled: isRunningInCi == false
-      ),
-      ciGoldensConfig: CiGoldensConfig(
-        enabled: isRunningInCi == true
-      ),
+    config: AlchemistConfig(
+      platformGoldensConfig:
+          PlatformGoldensConfig(enabled: isGithubActionsEnv == false),
+      ciGoldensConfig: CiGoldensConfig(enabled: isGithubActionsEnv == true),
     ),
     run: testMain,
   );
