fd5f71f39beb210ec90f82074b6ab83955f287b5
lesleysin <nikandsin@gmail.com>
2024-11-21 15:49:11 +0200

    Added test

---
+++ test/d_host_test.dart
@@ -0,0 +1,61 @@
+import "package:flutter/material.dart";
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_duit/flutter_duit.dart';
+
+void main() {
+  group("DuitHost widget tests", () {
+    group("Host error builder tests", () {
+      testWidgets("Has error builder", (WidgetTester tester) async {
+        final driver = DuitDriver.static(
+          {
+            "type": "invalid_type",
+            "id": "1",
+          },
+          transportOptions: HttpTransportOptions(),
+        );
+
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: DuitViewHost(
+              driver: driver,
+              errorWidgetBuilder: (context, error) {
+                return Text(error.toString());
+              },
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        final errorText = find.text(
+          "Invalid argument(s): Cant infer element type from json schema: invalid_type with id= 1",
+        );
+
+        expect(errorText, findsOneWidget);
+      });
+      testWidgets("Error builder undefined", (WidgetTester tester) async {
+        final driver = DuitDriver.static(
+          {
+            "type": "invalid_type",
+            "id": "1",
+          },
+          transportOptions: HttpTransportOptions(),
+        );
+
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: DuitViewHost(
+              driver: driver,
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        expect(tester.takeException(), isInstanceOf<ArgumentError>());
+      });
+    });
+  });
+}
