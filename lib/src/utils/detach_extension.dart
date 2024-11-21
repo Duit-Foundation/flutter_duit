4ee6ac682c92e727e6df85913055b8fd43b0efb4
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2024-11-21 15:53:47 +0200

    feat: error widget builder for DuitViewHost (#143)
    * Added errorWidgetBuilder property to DuitViewHost
    
    * Added test

--- lib/src/utils/detach_extension.dart
+++ lib/src/utils/detach_extension.dart
@@ -1,11 +1,5 @@
 import 'package:duit_kernel/duit_kernel.dart';
 
-// extension DetachExtension<T> on UIElementController<T> {
-//   void detach() {
-//     driver.detachController(id);
-//   }
-// }
-
 extension type DetachableController(UIElementController controller) implements UIElementController {
   void detach() {
     controller.driver.detachController(controller.id);
