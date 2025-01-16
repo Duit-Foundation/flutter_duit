7a19c33bcb99e2ec7f3e724af30ba083e23d2077
lesleysin <nikandsin@gmail.com>
2025-01-16 17:38:55 +0200

    wip

--- lib/src/utils/json_utils.dart
+++ lib/src/utils/json_utils.dart
@@ -1,6 +1,6 @@
 import 'package:duit_kernel/duit_kernel.dart';
 
-class JsonUtils {
+sealed class JsonUtils {
   ///Modifies the original [model] object by adding values from the [dataSource]
   ///object to it if there are [ValueReference] objects in the attributes
   static Map<String, dynamic> mergeWithDataSource(
@@ -25,5 +25,43 @@ class JsonUtils {
     }
 
     return model.data;
+  }
+
+  static T? nullOrParse<T>(
+    String key,
+    Map<String, dynamic> map,
+    T Function(Map<String, dynamic>) f,
+  ) {
+    if (map.containsKey(key)) {
+      return f(map[key]);
+    } else {
+      return null;
+    }
+  }
+
+  static T valueOrDefault<T>(
+    String key,
+    Map<String, dynamic> map,
+  ) {
+    if (map.containsKey(key)) {
+      return map[key];
+    } else {
+      return switch (T) {
+        String => "",
+        num => 0,
+        bool => false,
+        List() => const [],
+        Map() => const {},
+        Type() => throw UnimplementedError(),
+      } as T;
+    }
+  }
+
+  static void assertFields(Map<String, dynamic> map, Iterable<String> fields) {
+    for (var field in fields) {
+      if (!map.containsKey(field)) {
+        throw ArgumentError("Field $field is required");
+      }
+    }
   }
 }
