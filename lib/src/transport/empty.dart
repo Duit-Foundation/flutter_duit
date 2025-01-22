1504f58b1ca1359fac0b8aeccdfee027b67f0a3e
lesleysin <nikandsin@gmail.com>
2025-01-22 11:32:29 +0200

    Added mock transport impl

---
+++ lib/src/transport/empty.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter_duit/flutter_duit.dart';
+
+final class EmptyTransport implements Transport {
+  @override
+  Future<Map<String, dynamic>?> connect({Map<String, dynamic>? initialData}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void dispose() {}
+
+  @override
+  FutureOr<Map<String, dynamic>?> execute(
+      ServerAction action, Map<String, dynamic> payload) {
+    throw UnimplementedError();
+  }
+
+  @override
+  FutureOr<Map<String, dynamic>?> request(
+      String url, Map<String, dynamic> meta, Map<String, dynamic> body) {
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement url
+  String get url => throw UnimplementedError();
+}
--- lib/src/transport/index.dart
+++ lib/src/transport/index.dart
@@ -2,3 +2,4 @@ export 'http.dart';
 export 'ws.dart';
 export 'transport_type.dart';
 export 'native.dart';
+export 'empty.dart';
