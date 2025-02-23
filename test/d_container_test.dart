a068b0f8667a8fd73edbf7c9ebdc1b3fef017082
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-02-23 15:03:12 +0300

    Added test

---
+++ test/d_container_test.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_duit/flutter_duit.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    "DuitContainer widget tests",
+    () {
+      testWidgets("must build widget", (tester) async {
+        final driver = DuitDriver.static(
+          {
+            "type": "Container",
+            "id": "container",
+            "attributes": {
+              "width": 100,
+              "height": 100,
+              "decoration": {
+                "color": [
+                  0,
+                  0,
+                  0,
+                  1,
+                ],
+              }
+            },
+            "controlled": false,
+          },
+          transportOptions: HttpTransportOptions(),
+          enableDevMetrics: false,
+        );
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: DuitViewHost(
+              driver: driver,
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        final box = find.byKey(const ValueKey("container"));
+
+        expect(box, paints..rect(color: Colors.black));
+      });
+
+      testWidgets("must update widget", (tester) async {
+        final driver = DuitDriver.static(
+          {
+            "type": "Container",
+            "id": "container",
+            "attributes": {
+              "width": 100,
+              "height": 100,
+              "decoration": {
+                "color": [
+                  0,
+                  0,
+                  0,
+                  1,
+                ],
+              }
+            },
+            "controlled": true,
+          },
+          transportOptions: HttpTransportOptions(),
+          enableDevMetrics: false,
+        );
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: DuitViewHost(
+              driver: driver,
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        var box = find.byKey(const ValueKey("container"));
+
+        expect(box, paints..rect(color: Colors.black));
+
+        await driver.updateTestAttributes("container", {
+          "decoration": {
+            "color": "#FFFFFF",
+          }
+        });
+
+        await tester.pumpAndSettle();
+
+        box = find.byKey(const ValueKey("container"));
+
+        expect(box, paints..rect(color: Colors.white,));
+      });
+    },
+  );
+}
