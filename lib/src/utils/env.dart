3405fa9bbc52b43c2300cbad08050233b379cdd9
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-09-02 17:15:29 +0200

    fix

---
+++ lib/src/utils/env.dart
@@ -0,0 +1,12 @@
+/// Whether to throw an error when an unspecified widget type is encountered.
+///
+/// If set to true, an [ArgumentError] will be thrown when an unspecified widget
+/// type is encountered.
+///
+/// If set to false, a const [SizedBox] will be returned instead.
+///
+/// This is useful for debugging and development purposes.
+const throwOnUnspecifiedWidgetType = bool.fromEnvironment(
+  "D_THROW_ON_UNSPECIFIED_WIDGET_TYPE",
+  defaultValue: false,
+);
